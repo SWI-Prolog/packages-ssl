c6e1d86c4c410c103193e833bf6c6417bd7a6a78
Matt Lilley <matt.lilley@securitease.com>
2015-03-02 23:33:36 +1300

    Update pldoc

--- ssl.pl
+++ ssl.pl
@@ -308,43 +308,44 @@ ssl_context(Role, SSL, Module:Options) :-
 %	the  password  is  currently  only   supported  for  PEM  files.
 %	DER-encoded keys which are password protected will not load. The
 %	key must be an RSA key. EC, DH   and DSA keys are not supported,
-%	and PrivateKey will be bound to an  atom ('<ec key>', '<dh key>'
-%	or '<dsa key>') if you  try  and   load  such  a  key. Otherwise
-%	PrivateKey will be unified with key(KeyTerm)  where KeyTerm is a
-%	private_key/8 term representing a private key.
+%	and PrivateKey will be  bound  to   an  atom  (ec_key, dh_key or
+%	dsa_key) if you try and load   such  a key. Otherwise PrivateKey
+%	will be unified with privtate_key(KeyTerm)   where  KeyTerm is a
+%	rsa/8 term representing an RSA key.
 
 %%      load_public_key(+Stream, -PublicKey) is det.
 %
 %	Load a public key  PublicKey  from   the  given  stream  Stream.
 %	Supports loading both DER- and PEM-encoded keys. The key must be
 %	an RSA key. EC, DH and DSA keys are not supported, and PublicKey
-%	will be bound to an atom ('<ec key>', '<dh key>' or '<dsa key>')
-%	if you try and load such  a   key.  Otherwise  PublicKey will be
-%	unified with key(KeyTerm) where KeyTerm   is a public_key/5 term
-%	representing a publickey.
+%	will be bound to an atom (one   of ec_key, dh_key or dsa_key) if
+%	you try and load such a key. Otherwise PublicKey will be unified
+%	with  public_key(KeyTerm)  where  KeyTerm  is    an  rsa/8  term
+%	representing an RSA key.
 
-%%      rsa_private_decrypt(+PrivateKey, +CipherText, -PlainText) is det.
+%% rsa_private_decrypt(+PrivateKey, +CipherText, -PlainText) is det.
 %
-%	Decrypt a message using the   private key PrivateKey. CipherText
-%	is an atom containing the data   to  decrypt, and if successful,
-%	PlainText will be the decrypted data.  If decryption fails, then
-%	rsa_private_decrypt/3 will fail. This predicate  will be able to
-%	decrypt a message which was   encrypted via rsa_public_encrypt/3
-%	using the public key corresponding to PrivateKey.
+%	Decrypt  a  message  using  the   private  RSA  key  PrivateKey.
+%	CipherText is an atom containing  the   data  to decrypt, and if
+%	successful, PlainText will be the  decrypted data. If decryption
+%	fails, then rsa_private_decrypt/3 will fail. This predicate will
+%	be  able  to  decrypt  a  message    which   was  encrypted  via
+%	rsa_public_encrypt/3  using  the  public  key  corresponding  to
+%	PrivateKey.
 
 %%      rsa_private_encrypt(+PrivateKey, +PlainText, -CipherText) is det.
 %
-%	Encrypt a message using the private key PrivateKey. PlainText is
-%	an atom containing the  data  to   encrypt,  and  if successful,
-%	CipherText will be the encrypted data. If encryption fails, then
-%	rsa_private_encrypt/3 will fail. CipherText will   be able to be
-%	decrypted via rsa_public_decrypt/3 using the   public  key which
-%	corresponds to PrivateKey.
+%	Encrypt  a  message  using  the   private  RSA  key  PrivateKey.
+%	PlainText is an atom containing  the   data  to  encrypt, and if
+%	successful,  CipherText  will  be  the    encrypted   daQta.  If
+%	encryption  fails,  then   rsa_private_encrypt/3    will   fail.
+%	CipherText will be able to be decrypted via rsa_public_decrypt/3
+%	using the public key which corresponds to PrivateKey.
 
 %%      rsa_public_decrypt(+PublicKey, +CipherText, -PlainText) is det.
 %
-%	Decrypt a message using the public  key PublicKey. CipherText is
-%	an atom containing the  data  to   decrypt,  and  if successful,
+%	Decrypt a message using the public RSA key PublicKey. CipherText
+%	is an atom containing the data   to  decrypt, and if successful,
 %	PlainText will be the decrypted data.  If decryption fails, then
 %	rsa_public_decrypt/3 will fail. This predicate   will be able to
 %	decrypt a message which was  encrypted via rsa_private_encrypt/3
@@ -353,8 +354,8 @@ ssl_context(Role, SSL, Module:Options) :-
 
 %%      rsa_public_encrypt(+PublicKey, +PlainText, -CipherText) is det.
 %
-%	Encrypt a message using the public   key PublicKey. PlainText is
-%	an atom containing the  data  to   encrypt,  and  if successful,
+%	Encrypt a message using the public  RSA key PublicKey. PlainText
+%	is an atom containing the data   to  encrypt, and if successful,
 %	CipherText will be the encrypted data. If encryption fails, then
 %	rsa_public_encrypt/3 will fail. CipherText will   be  able to be
 %	decrypted via rsa_private_decrypt/3 using the  private key which
