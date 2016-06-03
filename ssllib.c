ca46bae1991dd7690c76398821783a0fc7802e03
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-06-03 22:33:00 +0200

    FIXED: Last patch didn't compile.  Wouter Beek.

--- ssllib.c
+++ ssllib.c
@@ -1627,7 +1627,7 @@ ssl_ssl_bio(PL_SSL *config, IOSTREAM* sread, IOSTREAM* swrite,
   { if ( config->pl_ssl_host )
       SSL_set_tlsext_host_name(instance->ssl, config->pl_ssl_host);
 #ifdef HAVE_X509_CHECK_HOST
-    X509_VERIFY_PARAM *param = SSL_get0_param((*instance)->ssl);
+    X509_VERIFY_PARAM *param = SSL_get0_param(instance->ssl);
     /* This could in theory be user-configurable. The documentation at
        https://wiki.openssl.org/index.php/Manual:X509_check_host(3)
        says that the flag is 'usually 0', however
