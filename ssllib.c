SWI-Prolog/packages-ssl

commit db2131e49d7ee28c1bcaaf0eb3b22d3fd87aadb6

ENHANCED: If SNI is used, certificate and key may be omitted.
In that case, if the SNI hook fails, the connection is rejected.

ssllib.c
@@ -956,6 +956,11 @@ ssl_cb_sni(SSL *s, int *ad, void *arg)
   if ( (servername = SSL_get_servername(s, TLSEXT_NAMETYPE_host_name)) != NULL )
     new_config = (config->pl_ssl_cb_sni)(config, servername);
 
+  if ( new_config == NULL &&
+       config->pl_ssl_certf == NULL &&
+       config->pl_ssl_certificate == NULL )
+    return SSL_TLSEXT_ERR_NOACK;
+
   SSL_set_SSL_CTX(s, new_config ? new_config->pl_ssl_ctx : config->pl_ssl_ctx );
 
   return SSL_TLSEXT_ERR_OK;
@@ -1462,11 +1467,13 @@ ssl_config(PL_SSL *config, term_t options)
   if ( config->pl_ssl_cert_required ||
        ( ( config->pl_ssl_certf || config->pl_ssl_certificate ) &&
          ( config->pl_ssl_keyf || config->pl_ssl_key ) ) )
-  { if ( config->pl_ssl_certf == NULL &&
-         config->pl_ssl_certificate == NULL )
+  { if ( config->pl_ssl_certf       == NULL &&
+         config->pl_ssl_certificate == NULL &&
+         config->pl_ssl_cb_sni      == NULL )
       return PL_existence_error("certificate", options);
-    if ( config->pl_ssl_keyf  == NULL &&
-         config->pl_ssl_key   == NULL )
+    if ( config->pl_ssl_keyf   == NULL &&
+         config->pl_ssl_key    == NULL &&
+         config->pl_ssl_cb_sni == NULL )
       return PL_existence_error("key_file", options);
 
     if ( config->pl_ssl_certf &&
@@ -1511,7 +1518,8 @@ ssl_config(PL_SSL *config, term_t options)
          SSL_CTX_use_RSAPrivateKey(config->pl_ssl_ctx, config->pl_ssl_key) <= 0 )
       return raise_ssl_error(ERR_get_error());
 
-    if ( SSL_CTX_check_private_key(config->pl_ssl_ctx) <= 0 )
+    if ( config->pl_ssl_key &&
+         SSL_CTX_check_private_key(config->pl_ssl_ctx) <= 0 )
     { ssl_deb(1, "Private key does not match certificate public key\n");
       return raise_ssl_error(ERR_get_error());
     }

ssl.pl
@@ -247,11 +247,13 @@ easily be used.
 %	  call(PredicateName, +SSL0, +HostName, -SSL)
 %	  ==
 %
-%	  Given the current context SSL0, and the host name of the client
-%	  request, the predicate computes SSL which is used as the
-%	  context for negotiating the connection. The first solution is
-%	  used.  If the predicate fails, the default options are used,
-%	  which are those of the encompassing ssl_context/3 call.
+%	  Given the current context SSL0, and the host name of the
+%	  client request, the predicate computes SSL which is used as
+%	  the context for negotiating the connection. The first solution
+%	  is used.  If the predicate fails, the default options are
+%	  used, which are those of the encompassing ssl_context/3
+%	  call. In that case, if no default certificate and key are
+%	  specified, the client connection is rejected.
 %
 %	@arg Role is one of `server` or `client` and denotes whether the
 %	SSL  instance  will  have  a  server   or  client  role  in  the

commit ad85904221b1e6eec934ab0a547f19f1c9d8c765

FIXED: Reject (again) cases where a private key *file* and SNI hook are specified, but the certificate is omitted.
db2131e49d7ee28c1bcaaf0eb3b22d3fd87aadb6 inadvertently relaxed the
check to cases where the private key was specified via key/1. However,
we want to raise this error also when key_file/1 is used instead.

ssllib.c
@@ -1512,8 +1512,7 @@ ssl_config(PL_SSL *config, term_t options)
       BIO_free(bio);
     }
 
-    if ( config->pl_ssl_key &&
-         SSL_CTX_check_private_key(config->pl_ssl_ctx) <= 0 )
+    if ( SSL_CTX_check_private_key(config->pl_ssl_ctx) <= 0 )
     { ssl_deb(1, "Private key does not match certificate public key\n");
       return raise_ssl_error(ERR_get_error());
     }